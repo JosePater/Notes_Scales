bffdfccb6f92e73d17000b6438a1821c311229d6
PaterDev <josepater_1130@hotmail.com>
2024-05-02 13:48:08 -0500

    feat: added guide table of musical notes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NoteComponent } from './components/note/note.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NoteComponent],
+  imports: [RouterOutlet, NoteComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
